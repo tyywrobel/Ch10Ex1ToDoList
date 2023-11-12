ea07fb8c78267895a04f123c4487e6cb121fff39
tyywrobel <tylerbeitel25@gmail.com>
2023-11-12 14:52:57 -0500

    Add project files.

---
+++ ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+        public Filters Filters { get; set; } = null!;
+        public List<Status> Statuses { get; set; } = null!;
+        public List<Category> Categories { get; set; } = null!;
+        public Dictionary<string, string> DueFilters { get; set; } = null!;
+        public List<ToDo> Tasks { get; set; } = null!;
+        public ToDo CurrentTask { get; set; } = null!; // used for Add
+    }
+}
